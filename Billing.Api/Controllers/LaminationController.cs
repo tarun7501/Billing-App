5a4c0054ef00d7c1234f02f997179ee4046de26e
tarun7501 <tarunkumar7501@gmail.com>
2026-02-06 14:21:09 +0530

    Added Required Initial APIs

---
+++ Billing.Api/Controllers/LaminationController.cs
@@ -0,0 +1,30 @@
+﻿using Billing.Api.Contracts;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Billing.Api.Controllers
+{
+    public class LaminationController : ControllerBase
+    {
+        private readonly ILaminationRepository _laminationRepository;
+
+        public LaminationController(ILaminationRepository laminationRepository)
+        {
+            _laminationRepository = laminationRepository;
+        }
+
+        [HttpGet("finishtypes")]
+        public async Task<IActionResult> GetFinshTypes()
+        {
+            var services = await _laminationRepository.GetLaminationFinishTypes();
+            return Ok(services);
+        }
+
+        [HttpGet("laminationtypes")]
+        public async Task<IActionResult> GetLaminationTypes()
+        {
+            var sizes = await _laminationRepository.GetLaminationTypes();
+            return Ok(sizes);
+        }
+    }
+}
---
+++ Billing.Api/Repositories/PhotoRepository.cs
@@ -0,0 +1,27 @@
+﻿using Billing.Api.Contracts;
+using Billing.Api.Domain.Entities;
+using Billing.Api.Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace Billing.Api.Repositories
+{
+	public class PhotoRepository : IPhotoRepository
+	{
+		private readonly BillingDbContext _dbContext;
+
+		public PhotoRepository(BillingDbContext dbContext)
+		{
+			_dbContext = dbContext;
+		}
+
+		public async Task<List<PhotoService>> GetPhotoServicesAsync()
+		{
+			return await _dbContext.PhotoServices.AsNoTracking().ToListAsync();
+		}
+
+		public async Task<List<PhotoSize>> GetPhotoSizesAsync()
+		{
+			return await _dbContext.PhotoSizes.AsNoTracking().ToListAsync(); 
+		}
+	}
+}
